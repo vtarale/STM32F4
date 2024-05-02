4cbef1f538b8e8da88b1d494ac4387eecdebbce4
vtarale <taralevihaan@gmail.com>
2024-05-02 19:51:41 +0530

    reducing code

--- src/include/stm32f407_adc.c
+++ src/include/stm32f407_adc.c
@@ -10,13 +10,8 @@ unsigned int start_convo(ADC *x);
 
 void set_adc(ADC *x, int res, int scan, int no_of_convo) {
     x->CR1 |= res << RES_BIT;
-
     if (scan == ON)
         x->CR1 |= MASK(SCAN_BIT);
-    else
-        x->CR1 &= ~MASK(SCAN_BIT);
-
-    x->CR2 &= ~MASK(ALIGN_BIT);
     SET_NO_OF_CONVERSIONS(x, (no_of_convo - 1));
 }
 
