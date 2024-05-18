cbc0705eacfcc6a46ebbd4c12e4f0a3f7acc86f1
vtarale <taralevihaan@gmail.com>
2024-05-18 13:20:48 +0530

    debugging dac

--- src/dac.c
+++ src/dac.c
@@ -5,12 +5,17 @@ Author: Vihaan Tarale
 #include "include/stm32f407.h"
 
 int start() {
-    set_dac(CHANEL_1, OFF);
+    set_dac(CHANEL_2, OFF);
     set_usart();
     char buffer[4096];
-    for (int counter = 4096; counter > 0; --counter){
+    write_dac(BIT_12, 4096, CHANEL_2);
+    while(1);
+    /*
+    for (int counter = 4096; counter > 0; counter=counter-10){
         send_string(itoa(counter, buffer, 10));
+        send_string("\n\r");
         write_dac(BIT_12, counter, CHANEL_1);
         delay(500000);
     }
+    */
 }
--- src/include/stm32f407_dac.c
+++ src/include/stm32f407_dac.c
@@ -13,37 +13,26 @@ void set_dac(int channel, int buffer) {
     if (channel == CHANEL_1) {
         set_moder(GPIOA, MODER_ANALOG, PIN_4);
         set_pupdr(GPIOA, PUPDR_NO_PULL_UP_DOWN, PIN_4);
-        DAC->CR = buffer << 17;
-        DAC->CR |= MASK(16);
-    } else if (channel == CHANEL_2) {
-        set_moder(GPIOA, MODER_ANALOG, PIN_5);
-        set_pupdr(GPIOA, PUPDR_NO_PULL_UP_DOWN, PIN_5);
         DAC->CR = buffer << 1;
         DAC->CR |= MASK(0);
     } else {
-        return;
+        set_moder(GPIOA, MODER_ANALOG, PIN_5);
+        set_pupdr(GPIOA, PUPDR_NO_PULL_UP_DOWN, PIN_5);
+        DAC->CR = buffer << 17;
+        DAC->CR |= MASK(16);
     }
 }
 
 void write_dac(int bit, int val, int channel) {
-    int buffer;
     if (channel == CHANEL_1) {
-        if (bit == BIT_12) {
-            DAC->DHR12R1 = 0x00;
+        if (bit == BIT_12)
             DAC->DHR12R1 = val << 0;
-        } else {
-            DAC->DHR8R1 = 0x00;
+        else
             DAC->DHR8R1 = val << 0;
-        }
-    } else if (channel == CHANEL_2) {
-        if (bit == BIT_12) {
-            DAC->DHR12R2 = 0x00;
-            DAC->DHR12R2 = val << 0;
-        } else {
-            DAC->DHR8R2 = 0x00;
-            DAC->DHR8R2 = val << 0;
-        }
     } else {
-        return;
+        if (bit == BIT_12)
+            DAC->DHR12R2 = val << 0;
+        else
+            DAC->DHR8R2 = val << 0;
     }
 }
