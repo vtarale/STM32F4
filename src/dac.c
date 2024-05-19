8543a915b64240e68aa99f488af82b91ca639122
vtarale <taralevihaan@gmail.com>
2024-05-19 19:10:38 +0530

    debugging

--- src/dac.c
+++ src/dac.c
@@ -8,14 +8,17 @@ int start() {
     set_dac(CHANEL_2, OFF);
     set_usart();
     char buffer[4096];
-    write_dac(BIT_12, 4096, CHANEL_2);
-    while(1);
-    /*
-    for (int counter = 4096; counter > 0; counter=counter-10){
+    unsigned int temp;
+    write_dac(BIT_12, 4095, CHANEL_2);
+    delay(500000);
+    for (int counter = 4095; counter > 0; counter=counter-10){
         send_string(itoa(counter, buffer, 10));
-        send_string("\n\r");
-        write_dac(BIT_12, counter, CHANEL_1);
+        write_dac(BIT_12, counter, CHANEL_2);
         delay(500000);
+        temp = DAC->DOR2;
+        send_string("\n\r");
+        send_string("BRUH:");
+        send_string(itoa(temp, buffer, 10));
+        send_string("\n\r");
     }
-    */
 }
--- src/include/stm32f407_dac.c
+++ src/include/stm32f407_dac.c
@@ -15,24 +15,36 @@ void set_dac(int channel, int buffer) {
         set_pupdr(GPIOA, PUPDR_NO_PULL_UP_DOWN, PIN_4);
         DAC->CR = buffer << 1;
         DAC->CR |= MASK(0);
+        DAC->CR = 3 << 2;
+        DAC->CR |= MASK(3);
     } else {
         set_moder(GPIOA, MODER_ANALOG, PIN_5);
         set_pupdr(GPIOA, PUPDR_NO_PULL_UP_DOWN, PIN_5);
         DAC->CR = buffer << 17;
         DAC->CR |= MASK(16);
+        DAC->CR = 3 << 19;
+        DAC->CR |= MASK(18);
     }
 }
 
 void write_dac(int bit, int val, int channel) {
     if (channel == CHANEL_1) {
-        if (bit == BIT_12)
-            DAC->DHR12R1 = val << 0;
-        else
-            DAC->DHR8R1 = val << 0;
+        if (bit == BIT_12) {
+            DAC->DHR12R1 = 0x00;
+            DAC->DHR12R1 = val & 0xFFF;
+        } else {
+            DAC->DHR8R1 = 0x00;
+            DAC->DHR8R1 = val & 0xFF;
+        }
+        DAC->SWTRIGR |= MASK(0);
     } else {
-        if (bit == BIT_12)
-            DAC->DHR12R2 = val << 0;
-        else
-            DAC->DHR8R2 = val << 0;
+        if (bit == BIT_12) {
+            DAC->DHR12R2 = 0x00;
+            DAC->DHR12R2 = val & 0xFFF;
+        } else {
+            DAC->DHR8R2 = 0x00;
+            DAC->DHR8R2 = val & 0xFF;
+        }
+        DAC->SWTRIGR |= MASK(0);
     }
 }
